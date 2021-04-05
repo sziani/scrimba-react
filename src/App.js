5e03212d8e9d6f7d537fa43efd9492f334dd6a57
Sami Ziani <zianisami0@gmail.com>
2021-04-05 18:34:56 +0200

    come back to chap 36

--- src/App.js
+++ src/App.js
@@ -7,16 +7,67 @@ import ToDoItem from "./components/ToDoItem";
 // import Joke from "./components/Joke";
 // import Product from "./components/Product"
 // import productsData from "./vschoolProducts"
-import todosData from "./todosData"
+import todosData from "./todosdata"
 
-function App() {
-    const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
+class App extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+        return {
+          todos: updatedTodos
+        }
+    })
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} handleChange={this.handleChange}/>)
     return (
-        <div>
-            {todoItems}
-        </div>
+      <div>
+        {todoItems}
+      </div>
     )
+  }
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       count: 0
+//     }
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(prevState => {
+//       return {
+//         count: prevState.count + 1
+//       }
+//     })
+//   }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.state.count}</h1>
+//                 <button onClick={this.handleClick}>Change!</button>
+//             </div>
+//         )
+//     }
+// }
+
 
 export default App
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function ToDoItem(props) {
   return (
       <div className="to-do-item">
-        <input type="checkbox" checked={props.item.completed} />
+        <input type="checkbox" checked={props.item.completed} onChange={() => {props.handleChange(props.item.id)}}/>
         <p>{props.item.text}</p>
       </div>
       );
